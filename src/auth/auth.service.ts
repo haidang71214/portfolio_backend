09fa0676e256765abd4aa3c302534c0551875518
haidang71214 <haidang71214@gmail.com>
2026-05-06 23:17:27 +0700

    setup cicd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: Prisma){}
+  constructor(private prisma: PrismaService){}
+
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
